06ab0d77de831366b5dbec56cf48926c54dd0477
ISPL\sumit.sharma <sumit2@amnex.com>
2025-04-21 13:28:59 +0530

    remove unwanted imports

--- src/main/java/com/amnex/fr_farmer_farm_sync/configuration/db/SecondaryDBConfig.java
+++ src/main/java/com/amnex/fr_farmer_farm_sync/configuration/db/SecondaryDBConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
